b6630b4fc55659aeec67df50546e4cbea03c9c86
jhuaehd <joshuaehd.silvio.37@gmail.com>
2022-07-12 14:19:18 +0800

    commit from vs code

--- connectToweb.py
+++ connectToweb.py
@@ -10,14 +10,14 @@ PATH = Service("C:\Program Files (x86)\chromedriver.exe")
 driver = webdriver.Chrome(service=PATH)
 
 # directly go to the provided url
-driver.get("techwithtim.net")
+driver.get("https://techwithtim.net")
 
 # gets the element name in the page and send following string to particular element name
 # driver.find_element("name", "s").send_keys("python")
-driver.find_element(By.NAME, value="s").send_keys("python")
-driver.find_element(By.NAME, value="s").send_keys(Keys.ENTER)
+# driver.find_element(By.NAME, value="s").send_keys("python")
 # driver.find_element(By.CSS_SELECTOR, value="type:submit").click()
-# driver.find_element(By.XPATH, value="//button[@type='submit']").click()
+driver.find_element(By.XPATH, value="//input[@type='search']").send_keys("python")
+# driver.find_element(By.NAME, value="//input[@type='submit']").send_keys(Keys.ENTER)
 
 # time.sleep(5)
 # driver.close()
