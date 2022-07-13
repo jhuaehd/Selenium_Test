a2631a041df7f2e07eea0651ab30afde892a9cc8
jhuaehd <joshuaehd.silvio.37@gmail.com>
2022-07-13 14:03:43 +0800

    COMMIT

--- playground.py
+++ playground.py
@@ -1,26 +1,41 @@
-import math
-import random
+# ======= FOR LOOP ===========
+# list1 = [1,3,5,6]
+# list2 = [2,4,7,8,1]
 
-    
+# for item in list1:
+#     if item in list2:
+#         print("Overlapping",item)
+#     else:
+#         list2.append(item)
 
-names = ['Cardo', 'JDC', 'Goku', 'Naruto', 'Luffy', 'Inuyasha', 'Kuroko', 'Rukawa']
-name = random.choice(names)
+# print(list2)
 
-print("List of Names: \n", names)
+# ========= ARRAY ============
+# import array as arr
+ 
+# # creating an array with integer type
+# a = arr.array('i', [1, 2, 3])
+ 
+# # printing original array
+# print ("The new created array is : ", end =" ")
+# for i in range (0, 3):
+#     print (a[i], end =" ")
+# print()
+ 
+# # creating an array with float type
+# b = arr.array('d', [2.5, 3.2, 3.3])
+ 
+# # printing original array
+# print ("The new created array is : ", end =" ")
+# for i in range (0, 3):
+#     print (b[i], end =" ")
 
-life = 7
-while life > 0:
-    user_input = input()
-    guess_name = user_input[0].capitalize() + user_input[1:]
-    if guess_name == name:
-        print("Awesome!! You guess the name")
-    else:
-        life = life - 1
-        print("Not the correct name")
-        print("Life: ",life)
-else:
-    print("Better luck next time.")
-    
 
+# ========= WHILE LOOP (REVERSE STRING) =================
+# a = [1, 2, 3, 4, 5, 6]
 
+# word = input()
+# word_list = list(word)
 
+# while word_list:
+#     print(word_list.pop())
