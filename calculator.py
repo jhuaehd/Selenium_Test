ab7ec85daf69e67f6b287632a8f3a0203a591134
jhuaehd <joshuaehd.silvio.37@gmail.com>
2022-07-15 09:08:04 +0800

    ADDDED COMMENTS

--- calculator.py
+++ calculator.py
@@ -31,19 +31,25 @@ oper = ['+', '-', '*', '/', '**', '%']
 user_operator = input()
 num2 = float(input())
 
+# ADDITION
 if user_operator == oper[0]:
     print("{} + {} = {:,.2f}".format(num1, num2, addition(num1, num2)))
+# SUBTRACTION
 elif user_operator == oper[1]:
     print("{} - {} = {:,.2f}".format(num1, num2, difference(num1, num2)))
+# MULTIPLICATION
 elif user_operator == oper[2]:
     print("{} * {} = {:,.2f}".format(num1, num2, product(num1, num2)))
+# DIVISION
 elif user_operator == oper[3]:
     if num2 != 0:
         print("{} / {} = {:,.2f}".format(num1, num2, quotient(num1, num2)))
     else:
         print("Undefine, You can't devide number with zero.")
+# POWER
 elif user_operator == oper[4]:
     print("{} ** {} = {:,.2f}".format(num1, num2, exponential(num1, num2)))
+# REMAINDER 
 elif user_operator == oper[5]:
     if num2 != 0:
         print("{} % {} = {:,.0f}".format(num1, num2, modulo(num1, num2)))
