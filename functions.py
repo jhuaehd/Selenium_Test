19aedcf13ca4cf00af762df261339ef8d89ad370
jhuaehd <joshuaehd.silvio.37@gmail.com>
2022-07-19 16:16:23 +0800

    commit

--- functions.py
+++ functions.py
@@ -97,13 +97,112 @@
 
 # ========= MULTIPLICATION TABLE ==============
 
-def multiplicationTable(column, row):
-    for r in range(1, row + 1):
-        print("\n", r, end="\t")
-        for c in range(2, column + 1):
-            print(c*r, end="\t")
+# def multiplicationTable(column, row):
+#     for r in range(1, row + 1):
+#         print("\n", r, end="\t")
+#         for c in range(2, column + 1):
+#             print(c*r, end="\t")
 
 
-column = int(input("Column: "))
-row = int(input("Row: "))
-multiplicationTable(column, row)
+# column = int(input("Column: "))
+# row = int(input("Row: "))
+# multiplicationTable(column, row)
+
+
+
+# ============ DICE SIMULATOR (TEXT BASED)================
+import random
+
+user1 = input("Player 1: ")
+user2 = input("Player 2: ")
+
+# dice values
+dice_value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+# rocker, paper, scissor for the first attempt
+p = ['rock', 'paper', 'scissor']
+
+while True:
+    winner = None
+    player1 = random.choice(p)
+    player2 = random.choice(p)
+    print(user1 + ": " + player1 + "  " + user2 + ": " + player1)
+    if player1 != player2:
+        print(user1 + ":" + player1  + "<---->"+ user2 + ":" + player2)
+        if player1 == 'rock' and player2 == 'paper':
+            winner = user2
+            print("Winner:",winner,"\n")
+        elif player1 == 'rock' and player2 == 'scissor':
+            winner = user1
+            print("Winner:",winner,"\n")
+        elif player1 == 'paper' and player2 == 'rock':
+            winner = user1
+            print("Winner:",winner,"\n")
+        elif player1 == 'paper' and player2 == 'scissor':
+            winner = user2
+            print("Winner:",winner,"\n")
+        elif player1 == 'scissor' and player2 == 'rock':
+            winner = user2
+            print("Winner:",winner,"\n")
+        elif player1 == 'scissor' and player2 == 'paper':
+            winner = user1
+            print("Winner:",winner,"\n")
+        break
+
+print("{} wins, {} will start first\n".format(winner[0].capitalize() + winner[1:], winner[0].capitalize() + winner[1:]))
+
+player1_score = 0
+player2_score = 0
+
+game  = True
+
+if winner == user2:
+    while game:
+        print("Please press enter to roll the dice")
+        enter = input("Player 2 turn _")
+        if player2_score < 100:
+            player2_dice = random.choice(dice_value)
+            player2_score += player2_dice
+            print("Player 1({}): ".format(user1) + str(player1_score) + "\n" +"Player 2({}): ".format(user2) + str(player2_score) + "(+{})".format(player2_dice) + "\n")
+        else:
+            print("Congrats " + user2)  
+            game = False
+
+        print("Please press enter to roll the dice")
+        enter = input("Player 1 turn _")
+        
+        if player1_score < 100:
+            player1_dice = random.choice(dice_value)
+            player1_score += player1_dice
+            print("Player 1({}): ".format(user1) + str(player1_score)  + "(+{})".format(player1_dice) + "\n" +"Player 2({}): ".format(user2) + str(player2_score) + "\n")
+        else:
+            print("Congrats " + user1)
+            game = False
+    else:
+        print("===================")
+        print("    Nice Game    ")
+else:
+    while game:
+        print("Please press enter to roll the dice")
+        enter = input("Player 1 turn _")
+        
+        if player1_score < 100:
+            player1_dice = random.choice(dice_value)
+            player1_score += player1_dice
+            print("Player 1({}): ".format(user1) + str(player1_score)  + "(+{})".format(player1_dice) + "\n" +"Player 2({}): ".format(user2) + str(player2_score) + "\n")
+        else:
+            print("Congrats " + user1)
+            game = False
+
+        print("Please press enter to roll the dice")
+        enter = input("Player 2 turn _")
+        if player2_score < 100:
+            player2_dice = random.choice(dice_value)
+            player2_score += player2_dice
+            print("Player 1({}): ".format(user1) + str(player1_score) + "\n" +"Player 2({}): ".format(user2) + str(player2_score) + "(+{})".format(player2_dice) + "\n")
+        else:
+            print("Congrats " + user2)  
+            game = False
+    else:
+        print("===================")
+        print("    Nice Game    ")
