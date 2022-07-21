29fb0d45ed9f56ca29a73285bf6756a7ff60c3bc
jhuaehd <joshuaehd.silvio.37@gmail.com>
2022-07-21 15:42:23 +0800

    added file

--- Jarvis.py
+++ Jarvis.py
@@ -1,24 +1,29 @@
 from datetime import date, datetime
 import geocoder
+from geopy.geocoders import Nominatim
 
 
 def get_currentTime():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M")
-    print("Sir, it's " + current_time)
+   now = datetime.now()
+   current_time = now.strftime("%I:%M %p")
+   print("Sir, it's " + current_time)
 
 def get_currentDate():
-    this_day = date.today()
-    day = int(this_day.strftime("%d"))
-    if day == 1 or day == 21 or day == 31:
-       print(this_day.strftime("It's %A, {}st of %b %Y ").format(day))
-    elif day == 2 or day == 22:
-       print(this_day.strftime("It's %A, {}nd of %b %Y ").format(day))
-    elif day == 3 or day == 23:
-       print(this_day.strftime("It's %A, {}rd of %b %Y ").format(day))
-    elif day > 3:
-       print(this_day.strftime("It's %A, {}th of %b %Y ").format(day))
+   this_day = date.today()
+   day = int(this_day.strftime("%d"))
+   if day == 1 or day == 21 or day == 31:
+      print(this_day.strftime("It's %A, {}st of %b %Y ").format(day))
+   elif day == 2 or day == 22:
+      print(this_day.strftime("It's %A, {}nd of %b %Y ").format(day))
+   elif day == 3 or day == 23:
+      print(this_day.strftime("It's %A, {}rd of %b %Y ").format(day))
+   elif day > 3:
+      print(this_day.strftime("It's %A, {}th of %b %Y ").format(day))
 
 def get_location():
-    g = geocoder.ip('me')
-    print(g.latlng)
+   geoLoc = Nominatim(user_agent="GetLoc")
+   g = geocoder.ip('me')
+   locname = geoLoc.reverse(g.latlng)
+   location = locname.address
+   print(location)
+
--- Min_Max.py
+++ Min_Max.py
@@ -1,4 +1,6 @@
 lst = [1, 2, 200, 10, 62, 5, 203, 6]
 
+highest = 0
 for l in lst:
+    
     
